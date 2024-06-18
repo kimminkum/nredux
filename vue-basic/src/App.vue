9f2598a13dc9925c3a9510703d1205e5c533bbbb
kimminkum <h24breaker@naver.com>
2024-06-18 09:53:26 +0900

    선언적 렌더링 & 클래스와 스타일바인딩 실습

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,35 +1,41 @@
 <template>
-  <div>{{ count }}</div>
-  <h1>Vue.js 라이프 사이클 테스트</h1>
+  <div>{{ rawHtml }}</div>
+  <!-- <div>{{ rawHtml2 }}</div> -->
+  <div>{{ rawHtml3 }}</div>
+  <h1 v-html="rawHtml3"></h1>
+
+  <h2 v-bind:class="{ active: isActive }">Hello Worlds!</h2>
+  <h2 :class="{ active: isActive }">Hello Worlds!</h2>
+  <button @click="change">버튼</button>
+
+  <h3 style="color: orange; font-size: 24px">스타일 바인딩 테스트 입니다.</h3>
+  <h3 :style="{ color: fontColor, fontSize: fontSize + 'px' }">
+    스타일 바인딩 테스트 입니다.
+  </h3>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      rawHtml: "이것은 우리의 죽음에 관한 이야기다.",
+      // rawHtml2: <span style="color:red">이렇게 쓰면 어떻게 되나</span>, 이건 안됨
+      rawHtml3: '<span style="color:red">이렇게 쓰면 어떻게 되나</span>',
+      isActive: false,
+      fontSize: 48,
+      fontColor: "orange",
     };
   },
-  beforeCreate() {
-    console.log("LifeCycle is beforeCreate", this.count);
-    // this.test();
-  },
-  created() {
-    console.log("LifeCycle is create", this.count);
-    this.test();
-  },
-  // beforeMount() {
-  //   console.log("LifeCycle is beforeMount", document.querySelector("h1"));
-  // },
-  // mounted() {
-  //   console.log("LifeCycle is mounted", document.querySelector("h1"));
-  // },
   methods: {
-    test() {
-      console.log("함수 호출!!");
+    change() {
+      this.isActive = !this.isActive;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+h2.active {
+  color: green;
+}
+</style>
