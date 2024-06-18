9ab4c20777fa40ecf91d030b58249365d00e213c
kimminkum <h24breaker@naver.com>
2024-06-18 09:38:35 +0900

    LifeCycle over by.9diin

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>{{ count }}</div>
+  <h1>Vue.js 라이프 사이클 테스트</h1>
 </template>
 
 <script>
@@ -8,6 +9,25 @@ export default {
     return {
       count: 0,
     };
+  },
+  beforeCreate() {
+    console.log("LifeCycle is beforeCreate", this.count);
+    // this.test();
+  },
+  created() {
+    console.log("LifeCycle is create", this.count);
+    this.test();
+  },
+  // beforeMount() {
+  //   console.log("LifeCycle is beforeMount", document.querySelector("h1"));
+  // },
+  // mounted() {
+  //   console.log("LifeCycle is mounted", document.querySelector("h1"));
+  // },
+  methods: {
+    test() {
+      console.log("함수 호출!!");
+    },
   },
 };
 </script>
