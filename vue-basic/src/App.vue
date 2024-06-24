bfc823e07532ee868366c3c91f6aee0846ab331c
kimminkum <h24breaker@naver.com>
2024-06-24 14:13:57 +0900

    watch over

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,71 +1,28 @@
 <template>
-  <div>
-    <div v-if="isVisible" class="red box"></div>
-    <div v-if="!isVisible" class="blue box"></div>
-    <div v-if="isVisible === true" class="green box"></div>
-    <div v-else class="black box"></div>
-    <div class="">
-      <li v-for="(item, index) in sampleArray" :key="item">
-        {{ item }} / {{ index }}
-      </li>
-    </div>
-    <div>
-      <li
-        v-for="(user, index) in otherArray"
-        :key="user.name"
-        @click="openDialog(index, user)"
-      >
-        {{ user.name }} / {{ user.id }} / {{ index }}
-      </li>
-    </div>
-  </div>
+  <button @click="changeMessage">{{ message }}</button>
+  {{ watchMessage }}
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isVisible: true,
-      sampleArray: ["a", "b", "c", "d", "e", "f"],
-      otherArray: [
-        { id: 0, name: "john" },
-        { id: 1, name: "doe" },
-        { id: 2, name: "hit" },
-      ],
+      message: "Hello, vue watch",
+      watchMessage: "",
     };
   },
+  watch: {
+    message() {
+      // window.alert("message변수에 담긴 데이터가 변경되었습니다. warning");
+      this.watchMessage = "Watch 동작";
+    },
+  },
   methods: {
-    openDialog: function (index, user) {
-      console.log(index);
-      console.log(user.id);
-      console.log(user.name);
+    changeMessage() {
+      this.message = "change";
     },
   },
 };
 </script>
 
-<style scoped>
-.box {
-  width: 100px;
-  height: 100px;
-}
-.red {
-  background-color: red;
-}
-.blue {
-  background-color: blue;
-}
-.green {
-  background-color: green;
-}
-.black {
-  background-color: black;
-}
-.flex {
-  display: flex;
-  justify-content: space-between;
-}
-li {
-  list-style: none;
-}
-</style>
+<style lang="scss" scoped></style>
